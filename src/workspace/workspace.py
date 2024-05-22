ae0bf9514a2f5b446ee1fecbbcf462603a3947ce
4equest <107108812+4equest@users.noreply.github.com>
2024-05-22 11:49:47 +0900

    hinagata (#3)

---
+++ src/workspace/workspace.py
@@ -0,0 +1,22 @@
+class WorkSpace:
+    def __init__(self) -> None:
+        pass
+    
+    def create_workspace(self) -> None:
+        pass
+    
+    def load_workspace(self) -> None:
+        pass
+    
+    def save_workspace(self) -> None:
+        pass
+    
+    def run_module(self) -> None:
+        pass
+    
+    def run_recipe(self) -> None:
+        pass
+    
+    def run_cmd(self) -> None:
+        pass
+    
